347ac7be0cb5609faae205348fbe24ce7c3a9e91
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-19 14:39:44 +0200

    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3
-
+"""a Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress.
 """
-a script that take an id of an user and return to list of tasks
-that he/she already done or not yet done using REST API
-"""
-
 import requests
 import sys
 
 
-if __name__ == '__main__':
-    user = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    response = requests.get(user)
-    j_resp = response.json()
-    print("Employee {} is done with ".format(j_resp.get('name')), end="")
-    todos = f'https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}'
-    result = requests.get(todos)
-    user_todos = result.json()
-    done_task = []
-    for task in user_todos:
-        if task.get('completed') is True:
-            done_task.append(task)
-    print("({}/{})".format(len(done_task), len(user_todos)))
-    for task in done_task:
-        print("\t{}".format(task.get('title')))
+if __name__ == "__main__":
+    employeeId = int(sys.argv[1])
+
+    api_url = "https://jsonplaceholder.typicode.com/todos/"
+
+    api_name_url = "https://jsonplaceholder.typicode.com/users/"
+
+    response = requests.get(api_url)
+    data = response.json()
+
+    name_response = requests.get(api_name_url)
+    name_data = name_response.json()
+
+    cc = 0
+    tc = 0
+
+    for i in range(len(data)):
+        if data[i]["userId"] is employeeId:
+            if (data[i]["completed"] is True):
+                cc += 1
+            tc += 1
+
+    u_name = name_data[employeeId - 1]["name"]
+
+    print("Employee {} is done with tasks({}/{}):".format(u_name, cc, tc))
+
+    for i in range(len(data)):
+        if data[i]["userId"] is employeeId:
+            if (data[i]["completed"] is True):
+                title = data[i]["title"]
+                print("\t", title)
