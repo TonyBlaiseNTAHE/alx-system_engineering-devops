d2dc95c48d2b92ba5d83f007e52b2c1a20161b5a
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-19 17:01:40 +0200

    a script that utilizes REST API to fetch a certain user and its datas

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,11 +7,33 @@ that he/she already done or not yet done using REST API
 
 import requests
 import sys
+import csv
 
 
 if __name__ == '__main__':
-    url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    user_id = sys.argv[1]
+    """ fetching username"""
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     res = requests.get(url)
     j_res = res.json()
     name = j_res.get('username')
-    print(name)
+
+    """ fetch all tasks for the user"""
+    todos = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    result = requests.get(todos)
+    user_todos = result.json()
+    all_tasks = []
+    for task in user_todos:
+        all_tasks.append([user_id,
+                          name,
+                          task.get('completed'),
+                          task.get('title')])
+
+    filename = f'{user_id}.cvs'
+    with open(filename, 'w', newline='') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=",",
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in all_tasks:
+            employee_writer.writerow(task)
