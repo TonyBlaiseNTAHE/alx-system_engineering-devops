665fbbbae88e984b77d198fdefb28c8e6a716a24
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-09 18:49:53 +0200

    function that returns 10 hot posts from reddit api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
-prints the titles of the first 10 hot posts
-listed for a given subreddit."""
+
+"""
+0-subs module
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
+    """ returns the number of subscribes from reddit api"""
+    if subreddit is None or type(subreddit):
+        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    header = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
+    params = {"limit": 10}
+    response = requests.get(url=url, headers=header, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    data = response.json().get("data")
+    for title in data.get("children"):
+        print(title.get('data').get('title'))
