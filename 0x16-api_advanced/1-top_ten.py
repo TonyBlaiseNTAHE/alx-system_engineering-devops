d0557df5b70df3cb0e1463bde2b52b7fefdf962d
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-09 18:02:17 +0200

    function that queries reddit api and returns the number of subscribers

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,8 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """ returns the number of subscribes from reddit api"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     header = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -18,4 +16,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 404:
         return 0
     data = response.json().get("data")
-    return data.get("subscribers")
+    return data.get("titles")
