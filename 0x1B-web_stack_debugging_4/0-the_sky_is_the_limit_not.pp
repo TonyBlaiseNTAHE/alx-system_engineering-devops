ce4d306080e1c49612577f73c705b57e98e98d8e
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-14 07:49:50 +0200

    fixing request limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,12 @@
-file {'/etc/default/nginx':
-  ensure  => file,
-  content => 'ULIMIT="-n 4096"',
+# fix request limit at nginx
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
