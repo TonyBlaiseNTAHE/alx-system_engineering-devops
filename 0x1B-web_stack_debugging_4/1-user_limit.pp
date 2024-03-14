1a011f1db3218b43ba87fa83fbacdc4abe713b3a
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-14 07:51:47 +0200

    os config

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,8 @@
-exec {'change_value_to_50':
-  command => "/bin/sed -i 's/5/50/g' /etc/security/limits.conf",
+# Increase sytem open files
+exec {'increase_hard':
+command => '/bin/sed -i "s/holberton hard nofile 5/holberton hard nofile 65535/g" /etc/security/limits.conf',
 }
 
-exec { 'change_value_to_40':
-  command => "/bin/sed -i 's/4/40/g' /etc/security/limits.conf",
+exec {'increase_soft':
+command => '/bin/sed -i "s/holberton soft nofile 4/holberton soft nofile 65535/g" /etc/security/limits.conf',
 }
